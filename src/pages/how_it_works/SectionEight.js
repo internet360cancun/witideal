06eb877ba954238c1c31345a94b3bc121fb75a30
JSegovia <acosta.segoviaa@gmamil.com>
2021-09-21 10:47:32 -0500

    Se agrega swiper

--- src/pages/how_it_works/SectionEight.js
+++ src/pages/how_it_works/SectionEight.js
@@ -1,56 +1,48 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import vive from '../../assets/howitworks/slides/vive.png';
-import remax from '../../assets/howitworks/slides/remax.png';
-import logo from '../../assets/howitworks/slides/logo.jpeg';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from 'swiper';
+import "swiper/swiper-bundle.css";
+import vive from "../../assets/howitworks/slides/vive.png";
+import remax from "../../assets/howitworks/slides/remax.png";
+import logo from "../../assets/howitworks/slides/logo.jpeg";
+SwiperCore.use([Autoplay]);
+
 
 const SectionEight = () => {
   return (
     <div className="container2">
-      <Carousel variant="dark">
-        <Carousel.Item>
+      <div className="BannerTitle8">Nuestros Aliados</div>
+      <Swiper loop={true} autoplay={true} >
+        <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <img src={vive} alt="" />
           </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <img src={remax} alt="" />
           </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <img src={logo} alt="" />
           </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <img src={vive} alt="" />
           </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={vive} alt="" />
-          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <img src={remax} alt="" />
           </div>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={logo} alt="" />
-          </div>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={vive} alt="" />
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={vive} alt="" />
-          </div>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={remax} alt="" />
-          </div>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={logo} alt="" />
-          </div>
-          <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
-            <img src={vive} alt="" />
-          </div>
-        </Carousel.Item>
-      </Carousel>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
