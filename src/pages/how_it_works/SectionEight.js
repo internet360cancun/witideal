0060718e907b316d49bd0f7d4f6e4e1e1d32becc
JSegovia <acosta.segoviaa@gmamil.com>
2021-09-24 10:16:14 -0500

    Se corrigue slider

--- src/pages/how_it_works/SectionEight.js
+++ src/pages/how_it_works/SectionEight.js
@@ -17,66 +17,70 @@ const SectionEight = () => {
   return (
     <div className="container2" id="section8">
       <div className="BannerTitle8">Nuestros Aliados</div>
-      <Swiper loop={true} autoplay={true}>
+      <Swiper
+        loop
+        autoplay
+        centeredSlides
+        breakpoints={{
+          576: { width: 576, slidesPerView: 2 },
+          768: { width: 768, slidesPerView: 2 },
+          992: { width: 992, slidesPerView: 3 },
+          1200: { width: 1200, slidesPerView: 4 },
+        }}
+      >
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://www.vivedelasrentas.com/" target="_blank">
-              <img src={vive} alt="" />
+              <img className="imgcontainer" src={vive} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://remax.com.mx/" target="_blank">
-              <img src={remax} alt="" />
+              <img className="imgcontainer" src={remax} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://legalglobalconsulting.com/lgc/" target="_blank">
-
-            <img src={lgc} alt="" />
+              <img className="imgcontainer" src={lgc} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://www.calmena.mx/" target="_blank">
-
-            <img src={calmena} alt="" />
+              <img className="imgcontainer" src={calmena} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://crediconsulting.com/" target="_blank">
-
-            <img src={crediconsulting} alt="" />
+              <img className="imgcontainer" src={crediconsulting} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://www.lgcrealestateschool.mx/" target="_blank">
-
-            <img src={lgcres} alt="" />
+              <img className="imgcontainer" src={lgcres} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="http://mundoinmobiliario.tv/" target="_blank">
-
-            <img src={mundoinmobiliario} alt="" />
+              <img className="imgcontainer" src={mundoinmobiliario} alt="" />
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="col-xs-3 col-sm-3 col-md-3 mt-5 mb-3">
             <a href="https://www.rematecasas.mx/" target="_blank">
-
-            <img src={remate} alt="" />
+              <img className="imgcontainer" src={remate} alt="" />
             </a>
           </div>
         </SwiperSlide>
