c2e61dfcc27e3ac47c76f42bbb8087f8dd77f516
JSegovia <acosta.segoviaa@gmamil.com>
2021-09-27 17:13:21 -0500

    Se desinstaló bootstrap

--- src/stripe/isRisingStar.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase/compat/app";
-
-export const isRisingStar = async () => {
-  await firebase.auth().currentUser.getIdToken(true);
-  const decodedToken = await firebase.auth().currentUser.getIdTokenResult();
- 
-
-  return decodedToken.claims.stripeRole ? true : false;
-};
--- src/stripe/isRockStar.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/compat/app';
-
-export const isRockStar = async () => {
-  await firebase.auth().currentUser.getIdToken(true);
-  const decodedToken = await firebase.auth().currentUser.getIdTokenResult();
-
-  return decodedToken.claims.stripeRole ? true : false;
-};
