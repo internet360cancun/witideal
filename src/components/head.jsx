f762dac3e4088ae1fce81bc3b60f53696aee9e64
JSegovia <acosta.segoviaa@gmamil.com>
2021-10-05 12:59:32 -0500

    meta tags

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -18,6 +18,7 @@ const Head = (props) => {
         content="Witideal, witideal, witi deal, huitidil, guitideal, guitidil, witi dil, witidil"
       />
       <link rel="canonical" href={window.location.href} />
+      
       <meta property="og:title" content={`Witideal | ${props.title}`} />
       <meta
         property="og:description"
@@ -26,10 +27,9 @@ const Head = (props) => {
           "Sitio de promoción de inmuebles en renta y venta. Encuentra el espacio que buscas en menos tiempo; comparte tu espacio al mejor precio."
         }
       />
-      <meta property="og:image" content={props.img} />
-
+      <meta property="og:image" content={props.img || ''} />
       <meta property="og:url" content={window.location.href} />
-      <meta property="og:site_name" content="Witideal | Portal inmobiliario" />
+      <meta property="og:site_name" content={`Witideal | ${props.title}`} />
     </Helmet>
   );
 };
