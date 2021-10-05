91cad47cbe6aa9ef3ec2c44f590f6693f8ae080d
JSegovia <acosta.segoviaa@gmamil.com>
2021-10-05 15:17:50 -0500

    og tags

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -24,10 +24,10 @@ const Head = (props) => {
         property="og:description"
         content={
           props.description ||
-          "Sitio de promoción de inmuebles en renta y venta. Encuentra el espacio que buscas en menos tiempo; comparte tu espacio al mejor precio."
+          "Sitio de Promoción de inmuebles en renta y venta. Encuentra el espacio que buscas en menos tiempo; comparte tu espacio al mejor precio."
         }
       />
-      <meta property="og:image" content={props.img || 'https://www.softzone.es/app/uploads/2018/04/guest.png?x=480&quality=20'} />
+      <meta property="og:image" content={props.img || 'https://imgur.com/a/s0k25tY'} />
       <meta property="og:url" content={window.location.href} />
       <meta property="og:site_name" content={`Witideal | ${props.title}`} />
     </Helmet>
