989dd8c6057b8383ba4df818ab06890158be3ee1
JSegovia <acosta.segoviaa@gmamil.com>
2021-12-06 12:34:54 -0500

    enlaces en categorias

--- src/pages/home/Categorias.js
+++ src/pages/home/Categorias.js
@@ -1,42 +1,51 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useMediaQuery } from "@material-ui/core";
+import { useHistory } from "react-router";
 
 const Categorias = () => {
+  const history = useHistory();
+
   const categorias = [
     {
       id: 1,
       title: "Inmuebles en venta",
+      link: "propiedades/Casa/Comprar/MX/CDMX",
       background:
         "https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/assets%2Ficons%2Fthumb%40inmuebles-ventas.png?alt=media&token=ec4b5d9f-e0e8-401e-acf5-e6cd038c1e25",
     },
     {
       id: 2,
       title: "Inmuebles en renta",
+      link: "propiedades/Casa/Rentar/MX/CDMX",
       background:
         "https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/assets%2Ficons%2Fthumb%40inmuebles-renta.png?alt=media&token=1f15eed7-9f5b-41a5-8f09-4208b370b954",
     },
     {
       id: 3,
       title: "Oficinas en renta",
+      link: "propiedades/Oficina/Rentar/MX/CDMX",
       background:
         "https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/assets%2Ficons%2Fthumb%40oficinas-renta.png?alt=media&token=5678533a-49c8-49d8-9d35-29368cfcff40",
     },
     {
       id: 4,
       title: "Propiedades comerciales en renta",
+      link: "",
       background:
         "https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/assets%2Ficons%2Fthumb%40propiedad-comercial.png?alt=media&token=5e0bac0b-8bba-4e1f-b763-a0f5dcb1eafc",
     },
     {
       id: 5,
       title: "Departamentos",
+      link: "propiedades/Departamento/Rentar/MX/CDMX",
       background:
         "https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/assets%2Ficons%2Fthumb%40departamento-renta.png?alt=media&token=4a2e74a1-1743-48eb-954c-bd84f4413f46",
     },
     {
       id: 6,
       title: "Terrenos",
+      link: "propiedades/Terreno/Comprar/MX/CDMX",
       background:
         "https://firebasestorage.googleapis.com/v0/b/witideal-b1f99.appspot.com/o/assets%2Ficons%2Fthumb%40terrenos.png?alt=media&token=822e8cd8-611a-41d4-be6e-92e588a5840e",
     },
@@ -44,6 +53,8 @@ const Categorias = () => {
 
   const tablet = useMediaQuery("(min-width:1200px)");
   const mobile = useMediaQuery("(min-width:500px)");
+
+  const handleToSearch = (link) => history.push(link);
 
   return (
     <div
@@ -68,6 +79,7 @@ const Categorias = () => {
             >
               {mobile ? (
                 <div
+                  onClick={() => handleToSearch(categoria.link)}
                   className="card-hover pointer"
                   style={{
                     backgroundImage: `linear-gradient(69.69deg, rgba(50, 255, 210, 0.9), rgba(65, 184, 249, 0.9)), url("${categoria.background}")`,
@@ -88,6 +100,7 @@ const Categorias = () => {
                 </div>
               ) : (
                 <div
+                  onClick={() => handleToSearch(categoria.link)}
                   className="card-hover pointer"
                   style={{
                     backgroundImage: `linear-gradient(69.69deg, rgba(50, 255, 210, 0.9), rgba(65, 184, 249, 0.9)), url("${categoria.background}")`,
