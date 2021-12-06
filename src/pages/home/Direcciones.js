78d2f6a63786551c384d473d1d908aa804c4d79d
JSegovia <acosta.segoviaa@gmamil.com>
2021-12-06 13:49:18 -0500

    direccioens map

--- src/pages/home/Direcciones.js
+++ src/pages/home/Direcciones.js
@@ -3,172 +3,137 @@ import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Direcciones = () => {
+  const direcciones = [
+    {
+      id: 1,
+      title: "Ubicaciones principales",
+      dir: [
+        {
+          id: 1,
+          dir: "Casa en del Valle",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Benito-Juárez/Col-del-Valle-Centro",
+        },
+        {
+          id: 2,
+          dir: "Casa en Condesa",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Cuauhtémoc/Colonia-Condesa",
+        },
+        {
+          id: 3,
+          dir: "Departamento en Condesa",
+          link: "propiedades/Departamento/Rentar/MX/CDMX/Cuauhtémoc/Colonia-Condesa",
+        },
+        {
+          id: 4,
+          dir: "Casa en Polanco",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Miguel-Hidalgo/Polanco-I-Secc",
+        },
+        {
+          id: 5,
+          dir: "Departamento en Polanco",
+          link: "propiedades/Departamento/Rentar/MX/CDMX/Miguel-Hidalgo/Polanco-I-Secc",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Propiedades en renta",
+      dir: [
+        {
+          id: 1,
+          dir: "Iztacalco CDMX",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Iztacalco",
+        },
+        {
+          id: 2,
+          dir: "Colonia del Valle",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Benito-Juárez/Col-del-Valle-Centro",
+        },
+        {
+          id: 3,
+          dir: "Santa Fe",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Álvaro-Obregón/Santa-Fe",
+        },
+        {
+          id: 4,
+          dir: "Jardines del Pedregal",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Álvaro-Obregón/Jardines-del-Pedregal",
+        },
+        {
+          id: 5,
+          dir: "Tlalpuente",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Tlalpan/San-Andrés-Totoltepec",
+        },
+        {
+          id: 6,
+          dir: "Bosques del Pedregal",
+          link: "propiedades/Casa/Rentar/MX/CDMX/Tlalpan/Bosques-del-Pedregal",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Propiedades en venta",
+      dir: [
+        {
+          id: 1,
+          dir: "Iztacalco CDMX",
+          link: "propiedades/Casa/Comprar/MX/CDMX/Iztacalco",
+        },
+        {
+          id: 2,
+          dir: "Colonia del Valle",
+          link: "propiedades/Casa/Comprar/MX/CDMX/Benito-Juárez/Col-del-Valle-Centro",
+        },
+        {
+          id: 3,
+          dir: "Santa Fe",
+          link: "propiedades/Casa/Comprar/MX/CDMX/Álvaro-Obregón/Santa-Fe",
+        },
+        {
+          id: 4,
+          dir: "Jardines del Pedregal",
+          link: "propiedades/Casa/Comprar/MX/CDMX/Álvaro-Obregón/Jardines-del-Pedregal",
+        },
+        {
+          id: 5,
+          dir: "Tlalpuente",
+          link: "propiedades/Casa/Comprar/MX/CDMX/Tlalpan/San-Andrés-Totoltepec",
+        },
+        {
+          id: 6,
+          dir: "Bosques del Pedregal",
+          link: "propiedades/Casa/Comprar/MX/CDMX/Tlalpan/Bosques-del-Pedregal",
+        },
+      ],
+    },
+  ];
+
   return (
     <Container className="py-5">
       <Row>
-        <Col md={4} className="d-flex justify-content-center">
-          <div>
-            <p className="subtitle py-3">Ubicaciones principales</p>
-            <div className="card-direcciones card-hover">
-              <div className="py-5 text-white text-left px-4 list-item">
-                <li>
-                  <Link
-                    className="text-white"
-                    to="propiedades/Casa/Rentar/MX/CDMX/Benito-Juárez/Col-del-Valle-Centro"
-                  >
-                    Casa en del Valle
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-white"
-                    to="propiedades/Casa/Rentar/MX/CDMX/Cuauhtémoc/Colonia-Condesa"
-                  >
-                    Casa en Condesa
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-white"
-                    to="propiedades/Departamento/Rentar/MX/CDMX/Cuauhtémoc/Colonia-Condesa"
-                  >
-                    Departamento en Condesa
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-white"
-                    to="propiedades/Casa/Rentar/MX/CDMX/Miguel-Hidalgo/Polanco-I-Secc"
-                  >
-                    Casa en Polanco
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-white"
-                    to="propiedades/Departamento/Rentar/MX/CDMX/Miguel-Hidalgo/Polanco-I-Secc"
-                  >
-                    Departamento en Polanco
-                  </Link>
-                </li>
+        {direcciones.map((direccion) => (
+          <Col
+            key={direccion.id}
+            md={4}
+            className="d-flex justify-content-center"
+          >
+            <div>
+              <p className="subtitle py-3">{direccion.title}</p>
+              <div className="card-direcciones card-hover">
+                <div className="py-5 text-white text-left px-4 list-item">
+                  {direccion.dir.map((dire) => (
+                    <li key={dire.id} className="right-hover">
+                      <Link className="text-white" to={dire.link}>
+                        {dire.dir}
+                      </Link>
+                    </li>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col md={4} className="d-flex justify-content-center">
-          <div>
-            <p className="subtitle py-3">Propiedades en renta</p>
-            <div className="card-direcciones card-hover">
-              <div className="py-5 text-white text-left px-4 list-item">
-                <li>
-                  <Link
-                    to="propiedades/Casa/Rentar/MX/CDMX/Iztacalco"
-                    className="text-white"
-                  >
-                    Iztacalco CDMX
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Rentar/MX/CDMX/Benito-Juárez/Col-del-Valle-Centro"
-                    className="text-white"
-                  >
-                    Colonia del Valle
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Rentar/MX/CDMX/Álvaro-Obregón/Santa-Fe"
-                    className="text-white"
-                  >
-                    Santa Fe
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Rentar/MX/CDMX/Álvaro-Obregón/Jardines-del-Pedregal"
-                    className="text-white"
-                  >
-                    Jardines del Pedregal
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Rentar/MX/CDMX/Tlalpan/San-Andrés-Totoltepec"
-                    className="text-white"
-                  >
-                    Tlalpuente
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Rentar/MX/CDMX/Tlalpan/Bosques-del-Pedregal"
-                    className="text-white"
-                  >
-                    Bosques del Pedregal
-                  </Link>
-                </li>
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col md={4} className="d-flex justify-content-center">
-          <div>
-            <p className="subtitle py-3">Propiedades en venta</p>
-            <div className="card-direcciones card-hover">
-              <div className="py-5 text-white text-left px-4 list-item">
-                <li>
-                  <Link
-                    to="propiedades/Casa/Comprar/MX/CDMX/Iztacalco"
-                    className="text-white"
-                  >
-                    Iztacalco CDMX
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Comprar/MX/CDMX/Benito-Juárez/Col-del-Valle-Centro"
-                    className="text-white"
-                  >
-                    Colonia del Valle
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Comprar/MX/CDMX/Álvaro-Obregón/Santa-Fe"
-                    className="text-white"
-                  >
-                    Santa Fe
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Comprar/MX/CDMX/Álvaro-Obregón/Jardines-del-Pedregal"
-                    className="text-white"
-                  >
-                    Jardines del Pedregal
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Comprar/MX/CDMX/Tlalpan/San-Andrés-Totoltepec"
-                    className="text-white"
-                  >
-                    Tlalpuente
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="propiedades/Casa/Comprar/MX/CDMX/Tlalpan/Bosques-del-Pedregal"
-                    className="text-white"
-                  >
-                    Bosques del Pedregal
-                  </Link>
-                </li>
-              </div>
-            </div>
-          </div>
-        </Col>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
